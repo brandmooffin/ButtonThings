c6d89dd49febd53463db195b98603d1604c0154e
Brandon Roberts <Brandon_Roberts@ultimatesoftware.com>
2020-02-24 17:23:08 -0500

    Updated library versions

--- mobile/src/main/java/apps/hackstermia/buttonthings/MainActivity.java
+++ mobile/src/main/java/apps/hackstermia/buttonthings/MainActivity.java
@@ -23,12 +23,13 @@ import android.content.ServiceConnection;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.ParcelUuid;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.HashSet;
 import java.util.List;
